a3984ee0c15ff40a8dacd07b6545f8b07c514360
Jieyieteoh03 <jieyie.teoh@lintramax.com>
2025-08-13 17:32:13 +0800

    Country Setup Api POST and GET

---
+++ tests/ApiTests/Master/CountrySetupAPI.spec.js
@@ -0,0 +1,59 @@
+const { test, expect } = require("@playwright/test");
+const token =
+  "lyeIrunY-2149ghq7s0q19wNagrJpqqUp-DUIrrzGSVSTomYcGtM6U2wYrA5xG1yJuQnizxf4Zqs4m4A3XAQjLlY2cqvWk3CcQi7F-mYYDi6eV5WPLj8cDgm1wXwcWO7GRujzCsm0eb2Tn5tFYhnQyqrUjU3gjUoiWSXMDPlq_77SqIjVnFP3XgGGLDbH4D5iTpMyfbFt17kCNbOcngQL78i1QiDEpWdXnDvdWP8mQs7ligKFHk1w-gRhS6wvwLgZvKnhc-nJv1r-P6NADMIzOs0LAlhGMwfYCKSjo8bSDXaHCG1Nx0n4zexyMc3t5DcdQ4G6F8M6vN9uWpwxQ3n4w7Fqf5V4pncQoV7zHjEw6BDGRiTGsYwhfvYz0kafykDUtW-hi06ukf_Tq2rT1g54bz0KP0";
+
+test.describe.serial("Country API", () => {
+  let ctryKey;
+
+  test("Add new Country", async ({ request }) => {
+    const response = await request.post(
+      "https://qa.quarto.cloud/zmasterapi/odata/Country",
+      {
+        data: {
+          CtryCode: "TEST123",
+          CtryDesc: "Country Create",
+          Active: true,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+        },
+      }
+    );
+
+    expect([200, 201]).toContain(response.status());
+
+    const res = await response.json();
+    ctryKey = parseInt(res.CtryKey);
+    console.log("Retrieved ctryKey:", ctryKey);
+  });
+
+  test("Get Country by CtryKey", async ({ request }) => {
+    console.log("Using ctryKey:", ctryKey);
+    const response = await request.get(
+      `https://qa.quarto.cloud/zmasterapi/odata/Country?key=${ctryKey}&$format=json`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+        },
+      }
+    );
+    console.log(await response.json());
+    expect(response.status()).toBe(200);
+  });
+
+  test("Get all Country", async ({ request }) => {
+    const response = await request.get(
+      "https://qa.quarto.cloud/zmasterapi/odata/Country?$orderby=CtryCode&$format=json&%24inlinecount=allpages&%24top=20&%24filter=Active%20eq%20true",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+        },
+      }
+    );
+    console.log(await response.json());
+    expect(response.status()).toBe(200);
+  });
+});
---
+++ tests/UiTests/CountrySetup.spec.js
@@ -0,0 +1,57 @@
+import { test, expect } from "@playwright/test";
+import LoginPage from "../../pages/Login";
+import navigateToForm from "../../functions/NavigateToForm";
+import SideMenuPage from "../../pages/SideMenu";
+import { InputPath, CreateData, EditData } from "../../data/masterData.json";
+import InputValues from "../../functions/InputValues";
+
+test.beforeEach(async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.login();
+});
+
+test("Create New Country Code", async ({ page }) => {
+  await navigateToForm(page, "Master File", "Country Setup", "General");
+
+  const sideMenu = new SideMenuPage(page);
+  await sideMenu.sideMenuBar.waitFor();
+  await sideMenu.btnNew.click();
+
+  // Define Elements and Values
+  const paths = InputPath.CountrySetupPath.split(",");
+  const columns = InputPath.CountrySetupColumn.split(",");
+  const values = CreateData.CountrySetupData.split(",");
+
+  // Input Data
+  if (paths.length == columns.length && columns.length == values.length) {
+    for (let i = 0; i < paths.length; i++) {
+      await InputValues(page, paths[i], columns[i], values[i]);
+    }
+  }
+
+  // Save Create
+  await sideMenu.btnSave.click();
+});
+
+// test("Edit New Country Code", async ({ page }) => {
+//   await navigateToForm(page, "Master File", "Country Setup", "General");
+
+//   const sideMenu = new SideMenuPage(page);
+//   await sideMenu.sideMenuBar.waitFor();
+//   await sideMenu.btnNew.click();
+
+//   // Define Elements and Values
+//   const paths = InputPath.CountrySetupPath.split(",");
+//   const columns = InputPath.CountrySetupColumn.split(",");
+//   const values = EditData.CountrySetupData.split(",");
+
+//   // Input Data
+//   if (paths.length == columns.length && columns.length == values.length) {
+//     for (let i = 0; i < paths.length; i++) {
+//       await InputValues(page, paths[i], columns[i], values[i]);
+//     }
+//   }
+
+//   // Save Edit
+//   await sideMenu.btnSave.click();
+// });

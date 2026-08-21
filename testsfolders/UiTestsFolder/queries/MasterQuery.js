6ee833037ac903bc5ec33013d2232c4c93195db4
Nicolas Chuang Hui Jiang <nicolas.chuang@lintramax.com>
2026-08-21 13:24:28 +0800

    Terrain Setup Automation

--- testsfolders/UiTestsFolder/queries/MasterQuery.js
+++ testsfolders/UiTestsFolder/queries/MasterQuery.js
@@ -800,6 +800,23 @@ function masterSQLCommand(formName) {
       WHERE A.TallPalmCode = @Code
       `;
       break;
+    
+    case "Terrain Setup":
+      sqlCommand = `
+      SELECT TerrainCode,
+      TerrainDesc,
+      CASE Active
+        WHEN 1 THEN 'True'
+        WHEN 0 THEN 'False'
+      END AS Active,
+      CASE RcdType
+        WHEN 0 THEN 'User'
+        WHEN 1 THEN 'System'
+      END AS RcdType
+      FROM GMS_TerrainStp
+      WHERE TerrainCode = @Code
+      `;
+      break;
 
     default:
       throw new Error(`Unknown formName: ${formName}`);
